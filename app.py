39644033c97eab41aeea39ce5795d84fd76362bf
waleed2590 <waleedrkhan90@gmail.com>
2022-05-07 17:19:08 +0500

    added send mail and add-webhook

--- app.py
+++ app.py
@@ -2,39 +2,63 @@ from flask import Flask
 from mailchimp_transactional.api_client import ApiClientError
 import mailchimp_transactional as MailchimpTransactional
 
+from flask_ngrok import run_with_ngrok
+
+
 app = Flask(__name__)
+# run_with_ngrok(app)
 
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 api_key = 'YpncKdxeRDTYtugALhMJ-Q'
-
 mailchimp = MailchimpTransactional.Client(api_key=api_key)
 message = {
-    "from_email": "manny@mailchimp.com",
-    "subject": "Hello world",
-    "text": "Welcome to Mailchimp Transactional!",
+    "from_email": "no-reply-waleed@mailchimp.com",
+    "subject": "Test App Mails",
+    "text": "These mails are specially for interview application.",
     "to": [
-      {
-        "email": "waleedrkhan90@gmail.com",
-        "type": "to"
-      }
+        {
+            "email": "waleedrkhan90@gmail.com",
+            "type": "to"
+        }
     ]
 }
 
 
+@app.route("/add-hook")
+def add_webhook():
+    try:
+        client = MailchimpTransactional.Client(api_key=api_key)
+        response = client.webhooks.add({"url": "https://c0e1-111-119-187-8.in.ngrok.io/", "description": "My Example Webhook",
+    "events": [
+        "send",
+        "open",
+        "click",
+        "hard_bounce",
+        "spam",
+        "reject"
+    ]})
+        print(response)
+    except ApiClientError as error:
+        print("An exception occurred: {}".format(error.text))
+        return "<p>Failed to add hook: {}</p>".format(error.text)
+    return "<p>Hook added successfully</p>"
+
+
+@app.route("/")
+def mandrill_response():
+    print("got a response from mandrill")
+    return "<p>Response</p>"
+
+
+@app.route("/send-mail")
 def send_mail():
     try:
         response = mailchimp.messages.send({"message": message})
         print('API called successfully: {}'.format(response))
     except ApiClientError as error:
         print('An exception occurred: {}'.format(error.text))
-
-
-send_mail()
+        return "<p>Failed with error : {}</p>".format(error.text)
+    return "<p>Success</p>"
+# send_mail()
 
 if __name__ == '__main__':
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run()
